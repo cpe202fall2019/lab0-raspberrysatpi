12d538b69af7f5d480dc8d3a43fee4b9ec8744ad
raspberrysatpi <raspberrysatpi@gmail.com>
2019-09-23 17:58:30 +0000

    Passed all test cases.

--- planets.py
+++ planets.py
@@ -1,7 +1,16 @@
+def main():
+
+	pass
+	weight_on_planets()
+	return
+
 def weight_on_planets():
-   # write your code here
+	
+	weight = float(input("What do you weigh on earth? "))
+	print("\nOn Mars you would weigh {0} pounds.\nOn Jupiter you would weigh {1} pounds.".format(weight*.38, weight*2.34))
+	return
    
    
    
 if __name__ == '__main__':
-   weight_on_planets()
+	main()
